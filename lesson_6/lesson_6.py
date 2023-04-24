2bd0263d5a397364cd45580c442fe69b3df616eb
EsenbekM <esenbekm03@gmail.com>
2023-04-24 21:04:21 +0600

    lesson 8

--- lesson_6/lesson_6.py
+++ lesson_6/lesson_6.py
@@ -35,19 +35,28 @@ result = re.sub(r'[Aai]', 'hello', text)
 
 # (0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.(1[0-9]{3}|20[0-2][0-9])
 
-with open('lesson_6/test_regs.txt', 'r', encoding='utf-8') as file:
-    content = file.read()
+# with open('lesson_6/MOCK_DATA.txt', 'r', encoding='utf-8') as file:
+    # content = file.readlines()
     # print(content)
 
-    beeline_list = re.findall(r"\+996 (?:77\d|22\d)[ \d]{9}", content)
-    print(beeline_list)
+    # beeline_list = re.findall(r"^.+", content)
+    # print(beeline_list)
 
-    megacom_list = re.findall(r"\+996 (?:55\d|99\d|75\d)[ \d]{9}", content)
-    print(megacom_list)
+    # megacom_list = re.findall(r"\+996 (?:55\d|99\d|75\d)[ \d]{9}", content)
+    # print(megacom_list)
 
-    nur_telecom_list = re.findall(r"\+996 (?:50\d|70\d)[ \d]{9}", content)
-    print(nur_telecom_list)
+    # nur_telecom_list = re.findall(r"\+996 (?:50\d|70\d)[ \d]{9}", content)
+    # print(nur_telecom_list)
 
     # fruit_list = re.findall(r'fruits\[\d+\] = [а-я"";]+', content)
     # for fruit in fruit_list:
     #     print(fruit)
+
+
+number = input()
+validator = re.match(r"\+996(?:50\d|70\d)[\d]{6}", number)
+if validator:
+    print("OK")
+    print(validator)
+else:
+    print("Wrong!")
